463070d1e7347f001ab6eed6890e9853871e86f0
Laura Martin <laura.martin@ably.com>
2023-07-25 13:35:23 +0100

    Slow down mouse moves
    Better simulate humans, so it's easier to guage when the experience
    becomes choppy.

--- tests/cursors.spec.ts
+++ tests/cursors.spec.ts
@@ -9,8 +9,11 @@ const WIDTH = 1600;
 // Number of seconds to run the test for
 const TIMEOUT = 30;
 
-// Number of seconds that we randomise jitter to
+// Number of seconds that we randomise jitter to between moves
 const MAX_JITTER = 2;
+
+// Number of increments between mouse moves to simulate human actions
+const INCREMENTS = 20;
 
 const viewportSize = {width: WIDTH, height: HEIGHT};
 
@@ -47,21 +50,49 @@ async function instanceTest(id: number, page: Page, end: number) {
   // Record our movements
   let movements = 0;
 
+  let start = newLocation();
   while (new Date().getTime() < end) {
-    await randomMove(page)
+    let end = newLocation();
+    await incrementalMove(page, start, end)
     movements++
+    start = end
   }
 
   console.log(`Instance ${id} finished with ${movements} movements`)
 }
 
-// Randomly move the cursor somewhere within the bounds of the window
-async function randomMove(page: Page) {
-  const targetWidth = randomTarget(WIDTH);
-  const targetHeight = randomTarget(HEIGHT);
-  await page.mouse.move(targetWidth, targetHeight);
+interface Location {
+  height: number,
+  width: number
+}
+
+function newLocation() : Location {
+  return {
+    height: randomTarget(HEIGHT),
+    width: randomTarget(WIDTH)
+  }
+}
+
+async function incrementalMove(page: Page, start: Location, end: Location) {
+  const incrementWidth = calcincrements(start.width, end.width);
+  const incrementHeight = calcincrements(start.height, end.height);
+
+  let width = start.width;
+  let height = start.width;
+  for (let i = 0; i < INCREMENTS; i++) {
+    width = width + incrementWidth;
+    height = height + incrementHeight;
+    await page.mouse.move(width, height);
+    await page.waitForTimeout(Math.floor(500 / INCREMENTS));
+  }
+  await page.mouse.move(end.width, end.height);
+
   await page.waitForTimeout(jitter());
 };
+
+function calcincrements(start: number, end: number) : number {
+  return Math.floor((end - start) / INCREMENTS)
+}
 
 function randomTarget(max: number) {
   return Math.floor(Math.random() * max)
