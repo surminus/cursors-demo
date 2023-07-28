54b900913795c6370858e3cf480cdf2356306d1e
Laura Martin <laura.martin@ably.com>
2023-07-28 12:54:17 +0100

    Draw a square

--- README.md
+++ README.md
@@ -7,9 +7,19 @@ Install:
 npm install
 ```
 
+Set up Ably API key in:
+```
+cp .env.example .env
+```
+
 Run tests:
 ```
 npm run test
 ```
 
-Open browser to http://localhost:5173 to see lots of cursors.
+Open browser to http://localhost:5173 to see cursors.
+
+Change the number of cursors:
+```
+MEMBERS=10 npm run test
+```
--- tests/cursors.spec.ts
+++ tests/cursors.spec.ts
@@ -1,39 +1,37 @@
 import { test, expect, Page, BrowserContext } from "@playwright/test";
 
 // Options
-// The number of simultaneous cursor moves. Each instance is a separate
+// The number of simultaneous cursor moves. Each member uses a separate
 // page.
-const INSTANCES = 5;
+const MEMBERS = process.env.MEMBERS || "5";
 
-// Height in pixels of window
+// Height in pixels of window (x)
 const HEIGHT = 1200;
-// Width in pixels of window
+// Width in pixels of window (y)
 const WIDTH = 1600;
 
-// Number of seconds to run the test for
-const TIMEOUT = 10;
+// How many squares to draw
+const SQUARES = 10;
+
+const SQUARE_SIZE = 600;
 
 // Number of seconds that we randomise jitter to between moves
 const MAX_JITTER = 2;
-
-// Number of increments between mouse moves to simulate human actions
-const INCREMENTS = 20;
 
 const viewportSize = { width: WIDTH, height: HEIGHT };
 
 test("default", async ({ context }) => {
   const now = new Date();
-  const end = now.getTime() + TIMEOUT * 1000;
   console.log(`Started at ${now}`);
 
   await Promise.all(
-    Array.from(Array(INSTANCES)).map((_, i) => instanceTest(i, context, end))
+    Array.from(Array(parseInt(MEMBERS))).map((_, i) => instanceTest(i, context))
   );
 
   console.log(`Finished at ${new Date()}`);
 });
 
-async function instanceTest(id: number, context: BrowserContext, end: number) {
+async function instanceTest(id: number, context: BrowserContext) {
   const page = await context.newPage();
 
   console.log(`Instance ${id} started`);
@@ -43,52 +41,38 @@ async function instanceTest(id: number, context: BrowserContext, end: number) {
   await page.goto("/");
   await expect(page).toHaveTitle(/Cursors/);
 
-  // Record our movements
-  let movements = 0;
-
-  let start = newLocation();
-  while (new Date().getTime() < end) {
-    let end = newLocation();
-    await incrementalMove(page, start, end);
-    movements++;
-    start = end;
+  for (let i = 0; i < SQUARES; i++) {
+    await square(page, SQUARE_SIZE);
   }
 
   console.log(
-    `Instance ${id} finished with ${movements * INCREMENTS} movements`
+    `Instance ${id} finished`
   );
 }
 
+async function square(page: Page, size: number = 500, steps: number = 100) {
+  await page.mouse.move(0, 0, {steps: 0});
+  await page.mouse.move(0, size, {steps});
+  await page.mouse.move(size, size, {steps});
+  await page.mouse.move(size, 0, {steps});
+  await page.mouse.move(0, 0, {steps});
+}
+
 interface Location {
-  height: number;
-  width: number;
+  x: number;
+  y: number;
 }
 
 function newLocation(): Location {
   return {
-    height: randomTarget(HEIGHT),
-    width: randomTarget(WIDTH),
+    x: randomTarget(HEIGHT),
+    y: randomTarget(WIDTH),
   };
 }
 
-async function incrementalMove(page: Page, start: Location, end: Location) {
-  const incrementWidth = calcincrements(start.width, end.width);
-  const incrementHeight = calcincrements(start.height, end.height);
-
-  let width = start.width;
-  let height = start.width;
-  for (let i = 0; i < INCREMENTS; i++) {
-    width = width + incrementWidth;
-    height = height + incrementHeight;
-    await page.mouse.move(width, height);
-    await page.waitForTimeout(Math.floor(500 / INCREMENTS));
-  }
-
+async function move(page: Page, location: Location, steps: number = 100) {
+  await page.mouse.move(location.x, location.y, {steps});
   await page.waitForTimeout(jitter());
-}
-
-function calcincrements(start: number, end: number): number {
-  return Math.floor((end - start) / INCREMENTS);
 }
 
 function randomTarget(max: number) {
